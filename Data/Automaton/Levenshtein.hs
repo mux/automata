083905a989af88ebf5719f647ee0a4826575e896
Maxime Henrion <mhenrion@gmail.com>
2011-01-03 18:47:24 +0100

    Use the list monad instead of concatMap in the Levenshtein automaton construction code to simplify it somewhat more.

--- Data/Automaton/Levenshtein.hs
+++ Data/Automaton/Levenshtein.hs
@@ -7,14 +7,13 @@ import Data.Automaton.NFA
 levenshtein :: Ord a => [a] -> Int -> NFA (Int,Int) a
 levenshtein term k = finalize $ foldr trans (unit (0,0)) moves
   where len      = length term
-        moves    = concatMap go (zip term [0..])
-         where go (c,i) = concatMap go' [0..k]
-                where go' j = ((i,j), Symbol c, (i+1,j)) :	-- Good input
-                              if j >= k then [] else
-                                [ ((i,j), Any, (i,j+1))		-- Deletion
-                                , ((i,j), Any, (i+1,j+1))	-- Substitution
-                                , ((i,j), Epsilon, (i+1,j+1))	-- Insertion
-                                ]
+        moves    = zip term [0..] >>= \(c,i) -> [0..k] >>= \j ->
+                     ((i,j), Symbol c, (i+1,j)) :	-- Good input
+                       if j >= k then [] else
+                         [ ((i,j), Any, (i,j+1))	-- Deletion
+                         , ((i,j), Any, (i+1,j+1))	-- Substitution
+                         , ((i,j), Epsilon, (i+1,j+1))	-- Insertion
+                         ]
         finalize nfa = foldr go nfa [0..k]
          where go i  = final (len, i) . if i >= k then id
                          else trans ((len,i), Any, (len,i+1))
