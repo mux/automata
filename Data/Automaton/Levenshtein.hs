eb80e223e757f2da198cfd7998f19f21b00fc58d
Maxime Henrion <mhenrion@gmail.com>
2011-01-03 13:57:44 +0100

    Slightly simplify the construction of a Levenshtein automaton.

--- Data/Automaton/Levenshtein.hs
+++ Data/Automaton/Levenshtein.hs
@@ -3,17 +3,18 @@ module Data.Automaton.Levenshtein where
 import Data.Automaton.NFA
 
 -- Probably only useful for strings, but I kept a type
--- allowing arbitrary 'Ord a' inputs anyway.
+-- allowing arbitrary 'Ord a' inputs, just in case.
 levenshtein :: Ord a => [a] -> Int -> NFA (Int,Int) a
-levenshtein term k = finalize . build $ unit (0,0)
-  where len          = length term
+levenshtein term k = finalize $ foldr trans (unit (0,0)) moves
+  where len      = length term
+        moves    = concatMap go (zip term [0..])
+        go (c,i) = concatMap go' [0..k]
+         where go' j = ((i,j), Symbol c, (i+1,j)) :	-- Good input
+                       if j >= k then [] else
+                         [ ((i,j), Any, (i,j+1))	-- Deletion
+                         , ((i,j), Any, (i+1,j+1))	-- Substitution
+                         , ((i,j), Epsilon, (i+1,j+1))	-- Insertion
+                         ]
         finalize nfa = foldr go nfa [0..k]
-          where go i = final (len, i) . if i >= k then id
+         where go i  = final (len, i) . if i >= k then id
                          else trans ((len,i), Any, (len,i+1))
-        build nfa    = foldr go nfa (zip term [0..])
-        go (c,i) nfa = foldr go' nfa [0..k]
-          where go' j = trans ((i,j), Symbol c, (i+1,j)) .	-- Good input
-                        if j >= k then id else
-                          trans ((i,j), Any, (i,j+1)) .		-- Deletion
-                          trans ((i,j), Any, (i+1,j+1)) .	-- Substitution
-                          trans ((i,j), Epsilon, (i+1,j+1))	-- Insertion
