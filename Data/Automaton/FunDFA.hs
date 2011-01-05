7e811ed287043d36f15c1f79e189e2dbdabbf8a9
Maxime Henrion <mhenrion@gmail.com>
2011-01-05 20:55:30 +0100

    Add a function-based DFA approach; basically it's just a record containing all the functions needed to implement the AcceptFA typeclass.

---
+++ Data/Automaton/FunDFA.hs
@@ -0,0 +1,19 @@
+module Data.Automaton.FunDFA where
+
+import Data.Automaton.Class
+
+-- A DFA where we don't store states nor transitions but rely on
+-- functions, just like in the mathematical definition of a DFA.
+
+data FunDFA s a =
+  FunDFA s			-- Initial state
+         (a -> s -> Maybe s)	-- The transition function
+         (s -> Bool)		-- Predicate for accepting states
+
+-- The simplest embodiment of the AcceptFA typeclass.
+instance AcceptFA (FunDFA s) a where
+  type StateType (FunDFA s) a = s
+
+  initial (FunDFA q0 _ _) = q0
+  step    (FunDFA _  s _) = s
+  final   (FunDFA _  _ f) = f
